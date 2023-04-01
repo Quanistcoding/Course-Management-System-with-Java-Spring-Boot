086f55a15c7b67084aac60e85fd8345bff7f29da
Chihhung Jen <quanistcoding@gmail.com>
2023-04-01 21:13:40 +0800

    Added create course functionality.

--- src/main/java/com/example/coursemanagementsystem/controller/CourseController.java
+++ src/main/java/com/example/coursemanagementsystem/controller/CourseController.java
@@ -1,6 +1,9 @@
 package com.example.coursemanagementsystem.controller;
 
+import com.example.coursemanagementsystem.entity.Course;
 import com.example.coursemanagementsystem.entity.Instructor;
+import com.example.coursemanagementsystem.repository.CourseRepository;
+import com.example.coursemanagementsystem.repository.InstructorRepository;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,25 +16,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/courses")
 public class CourseController {
+    private InstructorRepository instructorRepository;
+    private CourseRepository courseRepository;
+
+    public CourseController(InstructorRepository instructorRepository, CourseRepository courseRepository) {
+        this.instructorRepository = instructorRepository;
+        this.courseRepository = courseRepository;
+    }
 
     @GetMapping("")
     public String getListPage(Model model){
-
+        var courses = courseRepository.findAll();
+        model.addAttribute("courses",courses);
         return "courses/list";
     }
 
     @GetMapping("/create")
-    public String getCreatePage(@ModelAttribute Instructor instructor, Model model){
-        model.addAttribute("instructor",instructor);
-        return "instructors/create";
+    public String getCreatePage(@ModelAttribute Course course, Model model){
+        var instructors =  instructorRepository.findAll();
+        model.addAttribute("course",course);
+        model.addAttribute("instructors",instructors);
+        return "courses/create";
     }
 
     @PostMapping("/create")
-    public String handleCreate(@Valid Instructor instructor, BindingResult result, Model model){
-        if(result.hasErrors())
-            return "instructors/create";
+    public String handleCreate(@Valid Course course, BindingResult result, Model model){
+        System.out.println(course);
+        if(result.hasErrors()) {
+            var instructors =  instructorRepository.findAll();
+            model.addAttribute("instructors",instructors);
+            return "courses/create";
+        }
 
+        courseRepository.save(course);
 
-        return "redirect:/instructors";
+        return "redirect:/courses";
     }
 }
--- src/main/java/com/example/coursemanagementsystem/entity/Course.java
+++ src/main/java/com/example/coursemanagementsystem/entity/Course.java
@@ -1,6 +1,8 @@
 package com.example.coursemanagementsystem.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Course {
@@ -9,11 +11,76 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String StudentId;
-    private String lastName;
-    private String firstName;
+    @NotBlank
+    private String courseId;
+    @NotBlank
+    private String title;
+    private String description;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    public Course() {
+    }
+
+    public Course(String courseId, String title, String description) {
+        this.courseId = courseId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", courseId='" + courseId + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }
 }
+
+
