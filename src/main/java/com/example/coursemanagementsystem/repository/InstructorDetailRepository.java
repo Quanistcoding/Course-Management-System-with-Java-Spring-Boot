d7b73b0b760875966778e414aaeb76f3fe60c295
Chihhung Jen <quanistcoding@gmail.com>
2023-04-01 12:50:14 +0800

    Added instructor detail api.

---
+++ src/main/java/com/example/coursemanagementsystem/repository/InstructorDetailRepository.java
@@ -0,0 +1,9 @@
+package com.example.coursemanagementsystem.repository;
+
+import com.example.coursemanagementsystem.entity.InstructorDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+taut
+public interface InstructorDetailRepository extends JpaRepository<InstructorDetail,Integer> {
+}
--- src/main/java/com/example/coursemanagementsystem/repository/InstructorRepository.java
+++ src/main/java/com/example/coursemanagementsystem/repository/InstructorRepository.java
@@ -2,6 +2,7 @@ package com.example.coursemanagementsystem.repository;
 
 import com.example.coursemanagementsystem.entity.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface InstructorRepository extends JpaRepository<Instructor,Integer> {
 }
