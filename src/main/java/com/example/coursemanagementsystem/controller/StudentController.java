6073d8a143e236616370e66efc002b1a9b20b6e3
Chihhung Jen <quanistcoding@gmail.com>
2023-04-02 11:27:07 +0800

    Added delete student functionality.

--- src/main/java/com/example/coursemanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/coursemanagementsystem/controller/StudentController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/students")
@@ -45,6 +42,14 @@ public class StudentController {
 
         System.out.println(student);
         studentRepository.save(student);
+
+        return "redirect:/students";
+    }
+
+    @GetMapping("/{studentId}/delete")
+    public String handleDelete(@PathVariable int studentId){
+
+        studentRepository.deleteById(studentId);
 
         return "redirect:/students";
     }
